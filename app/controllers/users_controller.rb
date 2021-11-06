26886eb6e5b59bf49052e0630a246432c965de0f
Leandran <leandranm@outlook.com>
2021-11-06 19:35:54 +0200

    add stripe users and subscriptions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,26 @@ class UsersController < ApplicationController
 
   def info
     @subscription = current_user.subscription
+    if @subscription.active
+      @stripe_customer = Stripe::Customer.retrieve(
+        @subscription.stripe_user_id
+      )
+      @stripe_subscription = @stripe_customer.subscriptions.first
+    end
+
+  def charge
+    token = params["stripeToken"]
+    puts "token #{token}"
+    customer = Stripe::Customer.create(
+      source: token,
+      plan: 'custom',
+      email: current_user.email
+    )
+
+    current_user.subscription.stripe_user_id = customer.id
+    current_user.subscription.active = true
+    current_user.subscription.save
+
+    redirect_to users_info_path
   end
 end
